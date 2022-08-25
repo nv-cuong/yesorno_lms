c9ae335aadc4928f4f5b524eead6f0c4425390d3
longvh0599 <vulong051999@gmail.com>
2022-08-25 12:02:05 +0700

    merge fearute-hieu

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Course;
 use App\Models\ClassStudy;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class StudentController extends Controller
 {
