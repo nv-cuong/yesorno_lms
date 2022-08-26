cdbdb440d99665eb268598ae4e95c3cca7859ae9
Ngô Viết Đức <ngovietduc.b3@gmail.com>
2022-08-26 08:10:38 +0000

    Replace _alert.blade.php

--- resources/views/admin/_alert.blade.php
+++ resources/views/admin/_alert.blade.php
@@ -1,4 +1,12 @@
-@if(session('msg'))
+@if (session('message'))
+{{-- Conflic đừng xóa cái này nhé --}}
+    <div class="alert alert-{{ session('type_alert') }} alert-dismissible fade show" role="alert">
+        {{ session('message') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@elseif(session('msg'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
   {{ session('msg') }}
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
