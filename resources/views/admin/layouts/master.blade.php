3da5e7aae991c84d5986d23cc913a404f98d1aba
longvh0599 <vulong051999@gmail.com>
2022-08-31 10:35:58 +0700

    merge-feature-duc

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,11 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Team 5 | @yield('title')</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-=======
 
->>>>>>> b3934304045c4770715af860bed33a3cdf78b075
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -31,18 +29,11 @@
     <!-- Daterange picker -->
     <link rel="stylesheet" href="{{ asset('/admin/plugins/daterangepicker/daterangepicker.css') }}">
     <!-- summernote -->
-<<<<<<< HEAD
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/summernote/summernote-bs4.min.css') }}">
-    <!-- datatables -->
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('/admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }})">
-=======
     <link rel="stylesheet" href="{{ asset('/admin/plugins/summernote/summernote-bs4.min.css')}}">
     <!-- datatables -->
     <link rel="stylesheet" href="{{ asset('/admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }})">
->>>>>>> b3934304045c4770715af860bed33a3cdf78b075
+
     <link rel="stylesheet" href="{{ asset('/admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="{{ asset('admin/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
@@ -119,24 +110,6 @@
     <script src="{{ asset('/admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('/admin/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('/admin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<<<<<<< HEAD
-    <script src="{{ asset('/admin/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/jszip/jszip.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/pdfmake/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/pdfmake/vfs_fonts.js') }}"></script>
-    <!-- AdminLTE App -->
-
-    <script src="{{ asset('admin/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-    <script src="{{ asset('admin/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
-
-    <!-- SweetAlert2 -->
-    <script src="{{ asset('admin/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-    <script src="{{ asset('/ckeditor/ckeditor.js') }}"></script>
-
-     @yield('scripts')
-=======
     <script src="{{ asset('/admin/plugins/jszip/jszip.min.js') }}"></script>
     <script src="{{ asset('/admin/plugins/pdfmake/pdfmake.min.js') }}"></script>
     <script src="{{ asset('/admin/plugins/pdfmake/vfs_fonts.js') }}"></script>
@@ -149,7 +122,6 @@
     <script src="{{ asset('admin/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
     <script src="{{ asset('/ckeditor/ckeditor.js') }}"></script>
     @yield('scripts')
->>>>>>> b3934304045c4770715af860bed33a3cdf78b075
 </body>
 
 </html>
