52ad501d925cd0a3fdbaacb0d5a0b93a7fc342d8
longvh0599 <vulong051999@gmail.com>
2022-09-09 09:15:43 +0700

    add-learning

--- tests/Feature/App/Controller/Client/TestCoursesControllerTest.php
+++ tests/Feature/App/Controller/Client/TestCoursesControllerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Artisan;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+
 class TestCoursesControllerTest extends TestCase
 {
     private $user;
@@ -14,25 +15,41 @@ class TestCoursesControllerTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        Artisan::call('db:seed', ['--class'=>'DatabaseSeeder']);
+        Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
         $this->user = Sentinel::findUserById(4);
         Sentinel::login($this->user, true);
     }
 
-    public function test_index_make_test(){
-        
-        $response = $this->get(route('index_make',4));
+    public function test_index_make_test()
+    {
+
+        $response = $this->get(route('index_make', 4));
 
         $response->assertStatus(200);
 
         $response->assertSee('<h2> BÀi TEST CUỐI KHÓA </h2>', false);
     }
-    public function test_save_maked(){
+    public function test_save_maked()
+    {
         $testData = [
-            'q1'=>'abcdefgh',
+            'q1' => 'abcdefgh',
         ];
 
-        $response = $this->get(route('save_maked',[4,4]), $testData);
+        $response = $this->get(route('save_maked', [4, 4]), $testData);
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('user_test_answers', [
+            'answer' =>   NULL,
+        ]);
+    }
+
+    public function test_save_maked_error()
+    {
+        $testData = [
+            'q1' => [3, 4],
+        ];
+
+        $response = $this->get(route('save_maked', [4, 4]), $testData);
         $response->assertStatus(302);
 
         $this->assertDatabaseHas('user_test_answers', [
