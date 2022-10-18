88391707075c39559875a31b3c83644016d38f22
sant1ago-da-hanoi <contacts@sant1ago.dev>
2022-10-18 09:32:53 +0700

    document extension on frontend/dashboard

--- resources/views/admin/modules/courses/units/lessons/detail.blade.php
+++ resources/views/admin/modules/courses/units/lessons/detail.blade.php
@@ -6,12 +6,24 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Quản lí khóa học</h1>
+                    <h1>
+                        Quản lí khóa học
+                    </h1>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('course.index') }}">Khóa học</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('unit.detail', $lesson->unit_id) }}">Chương</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">{{ $lesson->title }}</li>
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('course.index') }}">
+                                    Khóa học
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('unit.detail', $lesson->unit_id) }}">
+                                    Chương
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                {{ $lesson->title }}
+                            </li>
                         </ol>
                     </nav>
                 </div>
@@ -58,7 +70,8 @@
                                                     <path
                                                         d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                                                 </svg>
-                                                <a href="{{ route('lesson.download', [$file->id]) }}" download="" style="color: white">
+                                                <a href="{{ route('lesson.download', [$file->id]) }}" download=""
+                                                    style="color: white">
                                                     @php echo " " . $file_name @endphp
                                                 </a>
                                             </button>
