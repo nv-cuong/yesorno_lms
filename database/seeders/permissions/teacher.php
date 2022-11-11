12788d8f8f23f1d2e273abed6a13e42fdaa16288
Nguyễn Cao Hoài Nam <contacts@sant1ago.dev>
2022-11-11 10:08:41 +0700

    permissions clarification

---
+++ database/seeders/permissions/teacher.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'dashboard'         => true,
+
+    // Users
+    'user.view'         => true,
+    'user.create'       => false,
+    'user.update'       => false,
+    'user.delete'       => false,
+
+    // Courses
+    'course.view'       => true,
+    'course.create'     => true,
+    'course.update'     => true,
+    'course.delete'     => false,
+
+    // Units
+    'unit.view'         => true,
+    'unit.create'       => true,
+    'unit.update'       => true,
+    'unit.delete'       => false,
+
+    // Questions
+    'question.view'     => true,
+    'question.create'   => true,
+    'question.update'   => true,
+    'question.delete'   => false,
+
+    // Tests
+    'test.view'         => true,
+    'test.create'       => true,
+    'test.update'       => true,
+    'test.delete'       => false,
+
+    // User Tests
+    'userTest.view'     => true,
+    'userTest.create'   => true,
+    'userTest.update'   => true,
+    'userTest.delete'   => false,
+];
