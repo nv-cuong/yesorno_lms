56129fc9ffaa14f44f136f2dec32f51a05d43161
longvh0599 <vulong051999@gmail.com>
2022-11-16 10:03:26 +0700

    edit_seeder_permisson

--- database/seeders/permissions/student.php
+++ database/seeders/permissions/student.php
@@ -2,40 +2,4 @@
 
 return [
     'dashboard'         => false,
-
-    // Users
-    'user.view'         => true,
-    'user.create'       => false,
-    'user.update'       => false,
-    'user.delete'       => false,
-
-    // Courses
-    'course.view'       => true,
-    'course.create'     => false,
-    'course.update'     => false,
-    'course.delete'     => false,
-
-    // Units
-    'unit.view'         => true,
-    'unit.create'       => false,
-    'unit.update'       => false,
-    'unit.delete'       => false,
-
-    // Questions
-    'question.view'     => true,
-    'question.create'   => false,
-    'question.update'   => false,
-    'question.delete'   => false,
-
-    // Tests
-    'test.view'         => true,
-    'test.create'       => false,
-    'test.update'       => false,
-    'test.delete'       => false,
-
-    // User Tests
-    'userTest.view'     => true,
-    'userTest.create'   => false,
-    'userTest.update'   => false,
-    'userTest.delete'   => false,
 ];
